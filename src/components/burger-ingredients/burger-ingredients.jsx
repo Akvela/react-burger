79a2ce87c36bf26c35f4ee5c6590585fa3912bcd
Akvela <akvela25@yandex.ru>
2022-05-10 22:43:51 +0300

    refactor: поправлены замечания после ревью

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -10,7 +10,7 @@ const BurgerIngredients = ({ ingredients, onIngredientClick }) => {
   const sauceArray = ingredients.filter(element => element.type === 'sauce');
   const mainArray = ingredients.filter(element => element.type === 'main');
   
-  const [current, setCurrent] = React.useState('one');
+  const [current, setCurrent] = React.useState('bun');
 
   const handleClick = (activeTab) => {
     setCurrent(activeTab);
@@ -21,22 +21,22 @@ const BurgerIngredients = ({ ingredients, onIngredientClick }) => {
   return (
     <div className={`${burgerIngredientsStyles.menu} pr-10`}>
       <div className={`${burgerIngredientsStyles.tab}`}>
-        <Tab value="one" active={current === 'one'} onClick={()=>{handleClick('one')}}>
+        <Tab value="bun" active={current === 'bun'} onClick={()=>{handleClick('bun')}}>
           Булки
         </Tab>
-        <Tab value="two" active={current === 'two'} onClick={()=>{handleClick('two')}}>
+        <Tab value="sauce" active={current === 'sauce'} onClick={()=>{handleClick('sauce')}}>
           Соусы
         </Tab>
-        <Tab value="three" active={current === 'three'} onClick={()=>{handleClick('three')}}>
+        <Tab value="main" active={current === 'main'} onClick={()=>{handleClick('main')}}>
           Начинки
         </Tab>
       </div>
       <div className={burgerIngredientsStyles.block}>
-        <h2 id='one' className='text text_type_main-medium pb-6 pt-10'>Булки</h2>
+        <h2 id='bun' className='text text_type_main-medium pb-6 pt-10'>Булки</h2>
         <Gallery itemList={bunsArray} handleClick={onIngredientClick} />
-        <h2 id='two' className='text text_type_main-medium pt-10 pb-6'>Соусы</h2>
+        <h2 id='sauce' className='text text_type_main-medium pt-10 pb-6'>Соусы</h2>
         <Gallery itemList={sauceArray} handleClick={onIngredientClick} />
-        <h2 id='three' className='text text_type_main-medium pt-10 pb-6'>Начинки</h2>
+        <h2 id='main' className='text text_type_main-medium pt-10 pb-6'>Начинки</h2>
         <Gallery itemList={mainArray} handleClick={onIngredientClick} />
       </div>
     </div>
